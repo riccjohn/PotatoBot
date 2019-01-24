9e3402e097e68699ef2ed6cf08f3539f006471af
riccjohn <riccjohn@gmail.com>
2019-01-24 13:19:07 -0600

    Add logging of guild channels

--- bot.js
+++ bot.js
@@ -3,12 +3,19 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log('Connected as ' + client.user.tag);
+  console.log('Servers:');
+  client.guilds.forEach((guild) => {
+      console.log(' - ' + guild.name)
+      guild.channels.forEach((channel) => {
+          console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+      })
+  })
+  const botChannel = client.channels.get('537707944083456029');
+  botChannel.send('PotatoBot, reporting for duty!');
+
 });
 
-// Get your bot's secret token from:
-// https://discordapp.com/developers/applications/
-// Click on your application -> Bot -> Token -> "Click to Reveal Token"
-const bot_secret_token =
+const botSecretToken =
   'NTM3NzA4MjkxNzQ1MTIwMjU3.DypMCA.lMzjkvCzMHAteDrCzjN4xo5Q_pg';
 
-client.login(bot_secret_token);
+client.login(botSecretToken);
